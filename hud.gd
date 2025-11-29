4b4115ca99e8af9bdef1e9d041ec5136b7da6d78
Tom Suworof <rc1995king@gmail.com>
2025-11-29 15:39:55 +0300

    Lesson 10 Extra Map info. Part 2: Best travel distance

--- hud.gd
+++ hud.gd
@@ -6,28 +6,36 @@ extends CanvasLayer
 @export var traveled_distance_label: Label
 @export var map_detector_count_label: Label
 
-@export var new_best_map_time_label: Label
+@export var new_best_map_scores_label: Label
 
 
 func _ready() -> void:
 	visible = false
-	new_best_map_time_label.visible = false
+	new_best_map_scores_label.visible = false
 
 
 func show_map_info(map: GameMap) -> void:
 	map_name_label.text = map.map_name
 	show_map_time(0.0)
 	visible = true
-	new_best_map_time_label.visible = false
+	new_best_map_scores_label.visible = false
 
 
 func show_map_time(time: float) -> void:
 	map_time_label.text = "%ds" % time;
 
 
-func show_new_best_map_time(new_time: int) ->void:
-	new_best_map_time_label.text = "New best time: %ds" % new_time
-	new_best_map_time_label.visible = true
+func show_new_best_map_scores(time: int, travel_distance: float) ->void:
+	var scores_txt := []
+	
+	if time >= 0:
+		scores_txt.push_back("New best time: %ds" % time)
+		
+	if travel_distance >= 0:
+		scores_txt.push_back("New best travel distance: %dpx" % travel_distance)
+
+	new_best_map_scores_label.text = "\n".join(scores_txt)
+	new_best_map_scores_label.visible = true
 
 
 func show_map_detector_count(valid: int, total: int) -> void:
--- map.gd
+++ map.gd
@@ -3,9 +3,11 @@ extends Node
 
 
 const KEY_BEST_TIME := "best_time"
+const KEY_BEST_TRAVEL_DISTANCE = "best_travel_distance"
 
 @export var map_name := "Map"
 
+var player_character: PlayerCharacter
 
 var registered_detectors_count := 0
 var valid_detectors_count := 0
@@ -19,6 +21,8 @@ func _ready() -> void:
 			var detector := child_node as Detector
 			registered_detectors_count += 1
 			detector.validity_changed.connect(_on_detector_validity_changed)
+		elif child_node is PlayerCharacter:
+			player_character = child_node
 			
 	Main.map_loaded(self)
 	HUD.show_map_info(self)
@@ -35,14 +39,31 @@ func _on_detector_validity_changed(valid: bool) -> void:
 		valid_detectors_count += 1
 		HUD.show_map_detector_count(valid_detectors_count, registered_detectors_count)
 		if valid_detectors_count == registered_detectors_count:
-			_update_best_time()
+			_update_best_scores()
 			Main.load_next_map()
 	else:
 		valid_detectors_count -= 1 
 	
 	
-func _update_best_time() -> void:
+func _update_best_scores() -> void:
 	var new_time := floori(current_map_time)
-	if not map_save_data.has(KEY_BEST_TIME) or new_time < map_save_data[KEY_BEST_TIME]:
+	if (
+		not map_save_data.has(KEY_BEST_TIME) 
+		or 
+		new_time < map_save_data[KEY_BEST_TIME]
+	):
 		map_save_data[KEY_BEST_TIME] = new_time
-		HUD.show_new_best_map_time(new_time)
+	else:
+		new_time = -1
+	
+	var new_travel_distance := floori(player_character.travel_distance)
+	if (
+		not map_save_data.has(KEY_BEST_TRAVEL_DISTANCE) 
+		or 
+		new_travel_distance < map_save_data[KEY_BEST_TRAVEL_DISTANCE]
+	):
+		map_save_data[KEY_BEST_TRAVEL_DISTANCE] = new_travel_distance
+	else:
+		new_travel_distance = -1
+
+	HUD.show_new_best_map_scores(new_time, new_travel_distance)
