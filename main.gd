8c0b71f749a68139801c1574741a8c8c066cb629
Tom Suworof <rc1995king@gmail.com>
2025-11-29 13:18:10 +0300

    Lesson 9 Game HUD. Part 2: Map time and best time

--- main.gd
+++ main.gd
@@ -8,13 +8,27 @@ const GAME_SAVE_PATH := "user://top-downer.save"
 const KEY_GAME_SAVE_VERSION := "version"
 const GAME_SAVE_VERSION := 1
 const KEY_MAP_PATH := "map_path"
+const KEY_PER_MAP_DATA := "per_map_data"
 
 
-var current_map_path: String
+var game_save_data: Dictionary
+
+
+func _ready() -> void:
+	if FileAccess.file_exists(GAME_SAVE_PATH):
+		var game_save_file := FileAccess.open(GAME_SAVE_PATH, FileAccess.READ)
+		game_save_data = game_save_file.get_var() as Dictionary
+		if not game_save_data.has(KEY_PER_MAP_DATA):
+			game_save_data[KEY_PER_MAP_DATA] = {}
+		if game_save_data[KEY_GAME_SAVE_VERSION] > GAME_SAVE_VERSION:
+			print("Tried to load unknown future save file format!")
+			game_save_data = {}
+	
+	game_save_data[KEY_GAME_SAVE_VERSION] = GAME_SAVE_VERSION
 
 
 func can_continue() -> bool:
-	return FileAccess.file_exists(GAME_SAVE_PATH)
+	return game_save_data.has(KEY_MAP_PATH)
 	
 
 func load_continue_map() -> void:
@@ -22,29 +36,23 @@ func load_continue_map() -> void:
 		print("Tried to continue without a save file!")
 		return
 	
-	var game_save_file := FileAccess.open(GAME_SAVE_PATH, FileAccess.READ)
-	var game_save_data := game_save_file.get_var() as Dictionary
+	var map_path := game_save_data[KEY_MAP_PATH] as String
 	
-	if game_save_data[KEY_GAME_SAVE_VERSION] == 1:
-		var map_path := game_save_data[KEY_MAP_PATH] as String
-		
-		if not map_path.begins_with(MAP_PATH_PREFIX) or map_path.contains(".."):
-			print("Unknown map path")
-			return
-		
-		load_map(map_path)
-	else:
-		print("Tried to load unknown save-file format")
+	if not map_path.begins_with(MAP_PATH_PREFIX) or map_path.contains(".."):
+		print("Unknown map path")
+		return
+	
+	load_map(map_path)
 
 
 func load_next_map() -> void:
-	var split_path := current_map_path.split('.')
+	var split_path := (game_save_data.get(KEY_MAP_PATH) as String).split('.')
 	var next_map_number := split_path[1].to_int() + 1
 	split_path[1] = str(next_map_number).pad_zeros(3)
 	var next_map_path := '.'.join(split_path)
 	
 	if not ResourceLoader.exists(next_map_path):
-		split_path[1] = '001'
+		split_path[1] = "001"
 		next_map_path = '.'.join(split_path)
 	
 	load_map(next_map_path)
@@ -65,11 +73,11 @@ func load_map(map_path: String) -> void:
 
 
 func map_loaded(map: GameMap) -> void:
-	current_map_path = map.scene_file_path
-	
+	var map_path := map.scene_file_path
+	game_save_data[KEY_MAP_PATH] = map_path
+	var per_map_data := game_save_data[KEY_PER_MAP_DATA] as Dictionary
+	if not per_map_data.has(map_path):
+		per_map_data[map_path] = {}
+	map.map_save_data = per_map_data[map_path]
 	var game_save_file := FileAccess.open(GAME_SAVE_PATH, FileAccess.WRITE)
-	game_save_file.store_var({
-		KEY_MAP_PATH: current_map_path,
-		KEY_GAME_SAVE_VERSION: GAME_SAVE_VERSION
-	})
-	
+	game_save_file.store_var(game_save_data)
--- map.gd
+++ map.gd
@@ -2,11 +2,15 @@ class_name GameMap
 extends Node
 
 
+const KEY_BEST_TIME := "best_time"
+
 @export var map_name := "Map"
 
 
 var registered_detectors_count := 0
 var valid_detectors_count := 0
+var current_map_time := 0.0
+var map_save_data: Dictionary
 
 
 func _ready() -> void:
@@ -18,13 +22,25 @@ func _ready() -> void:
 			
 	Main.map_loaded(self)
 	HUD.show_map_info(self)
+	
+	
+func _process(delta: float) -> void:
+	current_map_time += delta
+	HUD.show_map_time(current_map_time)
 
 
 func _on_detector_validity_changed(valid: bool) -> void:
 	if valid:
 		valid_detectors_count += 1
 		if valid_detectors_count == registered_detectors_count:
+			_update_best_time()
 			Main.load_next_map()
 	else:
 		valid_detectors_count -= 1 
 	
+	
+func _update_best_time() -> void:
+	var new_time := floori(current_map_time)
+	if not map_save_data.has(KEY_BEST_TIME) or new_time < map_save_data[KEY_BEST_TIME]:
+		map_save_data[KEY_BEST_TIME] = new_time
+		HUD.show_new_best_map_time(new_time)
